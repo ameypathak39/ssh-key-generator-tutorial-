b2efe3b120b174ffa7d9aed6ada2bd87f09f4c19
Ameya <125235218+ameypathak39@users.noreply.github.com>
2025-05-20 07:01:54 +0530

    Create simple-interest.sh

---
+++ simple-interest.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Placeholder file to meet course requirements.
+# The main content of this project is written in HTML.
+echo "This is an HTML project. No calculation logic present here."
+
+bash
+chmod +x simple-interest.sh
